afd3e578f1ae7b3df14c00791e0f74442914ee1c
Fredrik <fredrik0301@gmail.com>
2018-11-16 13:59:06 +0100

    view models tilføjet

---
+++ PantrTest/Models/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PantrTest.Models.ViewModels
+{
+    public class TransactionViewModel
+    {
+        public PostViewModel Post { get; set; }
+        public string Panter { get; set; } //skift type
+        public bool Collected { get; set; }
+        public bool Annulled { get; set; }
+    }
+}
