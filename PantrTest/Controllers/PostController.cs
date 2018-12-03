6703c26d87993ee4e4f108d062bac740f3d7e179
JonathanALenz <jona8772@stud.kea.dk>
2018-12-03 16:14:48 +0100

    merge confl

--- PantrTest/Controllers/PostController.cs
+++ PantrTest/Controllers/PostController.cs
@@ -23,7 +23,6 @@ namespace PantrTest.Controllers
         {
             using (PantrDatabaseEntities db = new PantrDatabaseEntities())
             {
-<<<<<<< HEAD
                 List<PostViewModel> posts = (from post in db.tbl_Post  
                              select new PostViewModel
                              {
@@ -59,7 +58,7 @@ namespace PantrTest.Controllers
                                  Completed = (bool)post.Completed,
                                  Date = post.Date.ToString()
                              }).ToList();
-=======
+
 
                 //List<JObject> lollern = new List<JObject>();
 
@@ -74,51 +73,6 @@ namespace PantrTest.Controllers
                 //}
 
                 //return lollern;
-
-
-                List<PostViewModel> posts = (from post in db.tbl_Post
-                                             select new PostViewModel
-                                             {
-                                                 Id = post.PK_Post,
-                                                 Material = new MaterialViewModel
-                                                 {
-                                                     Type = post.tbl_Material.Type
-
-                                                 },
-                                                 Giver = new UserViewModel
-                                                 {
-                                                     Firstname = post.tbl_User.Firstname,
-                                                     Surname = post.tbl_User.Surname,
-                                                     Phone = post.tbl_User.Phone,
-                                                     Email = post.tbl_User.Email,
-                                                     IsPanter = (bool)post.tbl_User.IsPanter,
-                                                     Address = new AddressViewModel()
-                                                     {
-                                                         Address = post.tbl_User.tbl_Address.Address,
-                                                         City = new CityViewModel()
-                                                         {
-                                                             City = post.tbl_User.tbl_Address.tbl_City.City,
-                                                             Zip = post.tbl_User.tbl_Address.tbl_City.Zip
-                                                         }
-                                                     }
-                                                 },
-
-                                                 PostQuantity = new PostQuantityViewModel
-                                                 {
-                                                     QuantityType = new QuantityTypeViewModel
-                                                     {
-                                                         //QuantityType = post.tbl_PostQuantity.tbl_QuantityType.QuantityType
-                                                     },
-                                                     //Quantity = (int)post.tbl_PostQuantity.Quantity
-                                                 },
-                                                 Address = post.Address,
-                                                 //StartTime = ConvertIntegerToTimeSpan((int)post.StartTime),
-                                                 //EndTime = ConvertIntegerToTimeSpan((int)post.EndTime),
-                                                 Claimed = (bool)post.Claimed,
-                                                 Completed = (bool)post.Completed,
-                                                 //Date = (DateTime)post.Date
-                                             }).ToList();
->>>>>>> anders
 
                 return posts;
             }
@@ -184,26 +138,13 @@ namespace PantrTest.Controllers
                     {
                         Type = postFromDb.tbl_Material.Type
                     },
-<<<<<<< HEAD
+
                     Quantity = postFromDb.Quantity,
                     Address = postFromDb.Address,
 
                     StartTime = (int)postFromDb.StartTime,
                     EndTime = (int)postFromDb.EndTime,
 
-=======
-                    PostQuantity = new PostQuantityViewModel
-                    {
-                        QuantityType = new QuantityTypeViewModel
-                        {
-                            //QuantityType = postFromDb.tbl_PostQuantity.tbl_QuantityType.QuantityType
-                        },
-                        //Quantity = (int)postFromDb.tbl_PostQuantity.Quantity
-                    },
-                    Address = postFromDb.Address,
-                    //StartTime = startTime,
-                    //EndTime = endTime,
->>>>>>> anders
                     Claimed = (bool)postFromDb.Claimed,
                     Completed = (bool)postFromDb.Completed,
                     Date = date.ToString("dd/MM/yyyy")
